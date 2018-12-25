88934df0011fa4f816116c1e43f7861418e78e84
NevMem <memlolkek@gmail.com>
2018-12-25 10:32:55 +0300

    A lot of changes in range slider

--- src/components/CustomRangeSlider.js
+++ src/components/CustomRangeSlider.js
@@ -11,32 +11,60 @@ export default class CustomRangeSlider extends Component {
     }
   }
 
-  dragStart(event) {
-    console.log(event)
+  getLeft = () => {
+    let left = (this.state.current - this.state.min) / (this.state.max - this.state.min)
+    left = 'calc(11px + (100% - 36px) / ' + (this.state.max - this.state.min)  + ' * ' +  (this.state.current - this.state.min) + ')'
+    return left
   }
 
-  onDrag(event) {
-    console.log(event)
+  dragStart = (event) => {
+    event.preventDefault()
+    document.addEventListener('mousemove', this.mouseMove)
+    document.addEventListener('mouseup', this.mouseUp)
   }
 
-  dragEnd(event) {
-    console.log(event)
+  mouseMove = (event) => {
+    event.stopPropagation()
+    let rect = this.slider.getClientRects()[0]
+    let workWidth = rect.width - 36
+    let currentMove = event.clientX - rect.x - 11;
+    if (currentMove < 0)
+      currentMove = 0
+    if (currentMove > workWidth)
+      currentMove = workWidth
+    this.setState({
+      current: this.state.min + ((this.state.max - this.state.min) * currentMove / workWidth | 0)
+    })
+  }
+
+  mouseUp = () => {
+    document.removeEventListener('mousemove', this.mouseMove)
+    document.removeEventListener('mouseup', this.mouseUp)
   }
 
   render() {
-    let left = (this.state.current - this.state.min) / (this.state.max - this.state.min)
-    left = 'calc(11px + (100% - 36px) / ' + (this.state.max - this.state.min)  + ' * ' +  (this.state.current - this.state.min) + ')'
+    let left = this.getLeft()
     return (
-      <div className='slider'>
-        <span
-          draggable
-          onDragStart={this.dragStart.bind(this)}
-          onDrag = {this.onDrag.bind(this)}
-          onDragEnd = {this.dragEnd.bind(this)}
-          className='thumb'
-          style = {{ left: left }}
-        />
-        <div className='track' />
+      <div className = 'range-slider-box'>
+        <div className = 'range-slider-heading'>
+          <div className = 'range-slider-min-value'>
+            {this.state.min}
+          </div>
+          <div className = 'range-slider-current-value'>
+            {this.state.current}
+          </div>
+          <div className = 'range-slider-max-value'>
+            {this.state.max}
+          </div>
+        </div>
+        <div ref = {rf => this.slider = rf } className='slider'>
+          <span
+            onMouseDown = {this.dragStart.bind(this)}
+            className='thumb'
+            style = {{ left: left }}
+          />
+          <div className='track' />
+      </div>
       </div>
     )
   }
