12a417c472f30567a3a78f813d3bd548454a8cc6
NevMem <memlolkek@gmail.com>
2019-01-01 20:50:03 +0300

    Bug fixed

--- src/components/EvaluationCard.js
+++ src/components/EvaluationCard.js
@@ -73,8 +73,8 @@ export default class EvaluationCard extends Component {
         <div className = 'evaluation-settings'>
           <CustomRangeSlider min = {200} max = {5000} current = {this.state.timelimit} handleChange = {this.handleTimeLimitChange.bind(this)} />
           <CustomCheckBox active = {this.state.using_O2} activate = {this.toggle_O2.bind(this)} caption = 'Use -O2' />
-          <CustomCheckBox active = {this.state.using_debug} activate = {this.toggle_using_debug.bind(this)} caption = 'Use -D_GLIBCXX_DEBUG' />
-          <CustomCheckBox active = {this.state.using_debug_pedantic} activate = {this.toggle_using_debug_pedantic.bind(this)} caption = 'Use -D_GLIBCXX_DEBUG_PEDANTIC' />
+          <CustomCheckBox active = {this.state.using_glibcxx_debug} activate = {this.toggle_using_debug.bind(this)} caption = 'Use -D_GLIBCXX_DEBUG' />
+          <CustomCheckBox active = {this.state.using_glibcxx_debug_pedantic} activate = {this.toggle_using_debug_pedantic.bind(this)} caption = 'Use -D_GLIBCXX_DEBUG_PEDANTIC' />
           <div>
             <CustomToggler toggled = {this.state.runTypeToggled} toggle = {this.toggleRunType.bind(this)} first_caption = 'single' second_caption = 'parallel' />
           </div>
